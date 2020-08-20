aff96220b4b58d522431497b89ea5362e2efef2b
prernabangad <email address>
2020-08-20 20:31:20 +0530

    Updates

---
+++ precache-manifest.83c042c75a48744b59580b8c2ac856ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f384a3f91c9e64d6f8f24ec76ba50cd3",
+    "url": "/counterapp/index.html"
+  },
+  {
+    "revision": "00d3b0a996cabbb8ac29",
+    "url": "/counterapp/static/css/main.86c757b1.chunk.css"
+  },
+  {
+    "revision": "631e961ecc420674d2cf",
+    "url": "/counterapp/static/js/2.d9093ec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counterapp/static/js/2.d9093ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d3b0a996cabbb8ac29",
+    "url": "/counterapp/static/js/main.9b945674.chunk.js"
+  },
+  {
+    "revision": "416aa84507bfe22ab290",
+    "url": "/counterapp/static/js/runtime-main.2d3c8383.js"
+  }
+]);
